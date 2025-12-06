68a6bea721c336749ce8ba06fb7593349c432356
Mayur Rajesh Sanap <mayursanap1997@gmail.com>
2025-12-05 19:37:57 -0500

    Add ESP32C3 firmware, amplify config, and iOS app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "iosappac0e3b7a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "iosAppS3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
